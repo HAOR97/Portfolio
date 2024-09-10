1878b7e4b26843008fdf8c34b406b91392fcc33a
HAOR97 <134957173+HAOR97@users.noreply.github.com>
2024-09-10 16:04:45 +0200

    added analitics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Projects from './sections/Projects/Projects'
 import Skills from './sections/Skills/Skills'
 import Contact from './sections/Contact/Contact'
 import Footer from './sections/Footer/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Contact />
       <Footer />
+      <Analytics />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./common/ThemeContext.jsx";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
